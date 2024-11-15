dda828a9778c34ff425a72cc70df0ab7394ce4bb
Chris Skinner <95317377+Chrisgr1@users.noreply.github.com>
2024-11-15 22:40:58 +0000

    updated filenames

--- app.py
+++ app.py
@@ -92,7 +92,7 @@ def main():
     print("Press Ctrl + C to exit.")
     
     # Create a small window to capture events
-    screen = pygame.display.set_mode((100, 100))
+    screen = pygame.display.set_mode((1000, 1000))
     running = True
     
     try:
